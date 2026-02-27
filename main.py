f7a829dff959bd4925a90cb94e2587f5ecaf6cc8
ololadejoseph497-afk <ololadejoseph497@gmail.com>
2026-02-27 21:15:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # --- CONFIGURATION ---
 # Replace the link below with your Google Sheet link (ending in /export?format=csv)
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1TIf1Z7R-bLeavsHfC6GLoszM42N1iblpVL6s-ZLaFUU/edit?usp=drivesdk"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1TIf1Z7R-bLeavsHfC6GLoszM42N1iblpVL6s-ZLaFUU/export?format=csv"
 PAYSTACK_LINK = "https://paystack.shop/pay/vantagepro-ai"
 
 def check_access(user_key):
